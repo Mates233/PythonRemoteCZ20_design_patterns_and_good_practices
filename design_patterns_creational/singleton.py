a1ca293671c138b4838cea181910a55106d99e83
mkrakovsky <michal.krakovsky@blindspot.ai>
2024-05-17 17:43:19 +0200

    feat(Docs): Added readme and new task

--- design_patterns_creational/singleton.py
+++ design_patterns_creational/singleton.py
@@ -1,4 +1,7 @@
 class ConfigSingleton:
+    # Všimněte si, že potřebujeme proměnnou na úrovni třídy, a ne proměnnou instance.
+    # Více o class variables vs instance variables zde:
+    # https://medium.com/@pouyahallaj/class-vs-instance-variables-in-python-5573e71c99b5
     _instance = None
 
     def __init__(self):
@@ -6,8 +9,9 @@ class ConfigSingleton:
 
     def __new__(cls):
         if cls._instance is None:
+            # Řádek 14 vytvoří objekt naší třídy, rozdíl medzi new a init:
+            # https://builtin.com/data-science/new-python
             cls._instance = super(ConfigSingleton, cls).__new__(cls)
-            # Initialize any attributes of the singleton here, if necessary
             cls._instance.value = "Default Configuration"
         return cls._instance
 
